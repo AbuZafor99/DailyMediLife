edcb0692eecb38a1c4ee378b0422d0f610c837c6
MD. Shahriar Nur <mdshahriarnur8105@gmail.com>
2025-05-08 13:19:18 +0600

    fun added

---
+++ app/src/main/java/com/example/knowledgecheck/TicTacToeActivity.java
@@ -0,0 +1,12 @@
+package com.example.knowledgecheck;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TicTacToeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ticktacktoe);
+    }
+}
